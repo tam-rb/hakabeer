7bc65fd30cce3e4a1c7a5f0ce3e58d62c30ba899
ThanhTam Nguyen <ThanhTam.Nguyen@nintex.com>
2019-04-20 20:06:50 +0800

    add firebase authenticate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { ProductListComponent } from './inside/products/product-list.component';
+import { UserRowComponent } from './shared/user-row/user-row.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'product', component: ProductListComponent},
+  {path: 'product', component: UserRowComponent, outlet:'userrow'},  
+  {path: '', component: UserRowComponent, outlet:'userrow'},
   {path: '**', component: HomeComponent }
 ];
 
