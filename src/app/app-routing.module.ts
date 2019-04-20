825da4255d5c9ae98deffedd0a32b9d5158e3e41
ThanhTam Nguyen <ThanhTam.Nguyen@nintex.com>
2019-04-20 18:52:18 +0800

    basic site structure and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { ProductListComponent } from './inside/products/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product', component: ProductListComponent},
+  {path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
---
+++ src/app/shared/user-row/user-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-row',
+  templateUrl: './user-row.component.html',
+  styleUrls: ['./user-row.component.css'],
+})
+export class UserRowComponent {
+ 
+}
