8fa77d917b2bb8a0d70844cffd412e183ef30d14
ThanhTam.Nguyen <thanhtam.nguyen@nintex.com>
2019-04-27 10:22:39 +0800

    order calculation fuction

--- src/app/inside/orders/order-edit/order-edit.component.ts
+++ src/app/inside/orders/order-edit/order-edit.component.ts
@@ -12,29 +12,54 @@ export class OrderEditComponent implements OnInit {
   orderForm : FormGroup;
   errorMessage: string
 
-  constructor(private fb:FormBuilder, private orderService: OrderService, private route:ActivatedRoute) { }
+  constructor(private fb:FormBuilder, private orderService: OrderService, private route:ActivatedRoute) { 
+
+  }
 
   ngOnInit() {
     this.orderForm = this.fb.group({
       table:['', [Validators.required]],
       pax: '',
       //promo: '',
-      //items: this.fb.array([
-      //  this.buildItems()
-      //])
+      total: '',
+      items: this.fb.array([
+        this.buildItems()
+      ])
     });
+    this.onChanges();
+
   }
 
   buildItems() : FormGroup {
     return this.fb.group({
       productName: '',
-      price: Number
+      quantity: '',
+      price: ''
     })
   }
   get items(): FormArray{
     return <FormArray>this.orderForm.get('items');
   }
 
+  addItem(){
+    this.items.push(this.buildItems());
+  }
+
   getErrorMessage(){}
 
+  onChanges(): void {
+    this.orderForm.valueChanges.subscribe(val =>{
+     this.orderForm.patchValue({
+       total: this.totalBill()
+     }, { emitEvent: false })
+    })
+  }
+
+  totalBill(): number {
+    let total = 0;
+    for(let item of this.items.value){
+      total += +item['quantity']*(+item['price']);
+    }
+    return total;
+  }
 }
