393bb6275f98c10ca018ec320d88cc9dae148e3f
ThanhTam Nguyen <mr.tamnt@gmail.com>
2019-05-17 09:15:13 +0800

    public page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyArXjeoHm6D8O02Zm5ZOCLRCgRXBotUTLM",
-    authDomain: "l-es6-01.firebaseapp.com",
-    databaseURL: "https://l-es6-01.firebaseio.com",
-    projectId: "l-es6-01",
-    storageBucket: "l-es6-01.appspot.com",
-    messagingSenderId: "1066705098860"
+    apiKey: "AIzaSyAoWCrjzPPeqWzJBZi5qR0JWSgrtaIXZr4",
+    authDomain: "hakabeer-01.firebaseapp.com",
+    databaseURL: "https://hakabeer-01.firebaseio.com",
+    projectId: "hakabeer-01",
+    storageBucket: "hakabeer-01.appspot.com",
+    messagingSenderId: "94423631585",
+    appId: "1:94423631585:web:c2f7df356c944dfb"
   }
 };
