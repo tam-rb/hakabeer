ca72bac43a7a4ad9d7d3ae3975a7466ad7023d89
ThanhTam.Nguyen <mr.tamnt@gmail.com>
2019-08-28 05:21:16 +0800

    merged orders

--- src/app/inside/products/goodsreceipt/goodsreceipt.component.ts
+++ src/app/inside/products/goodsreceipt/goodsreceipt.component.ts
@@ -22,40 +22,48 @@ export class GoodsreceiptComponent implements OnInit {
 
   mergeProduct()
   {
-
+    this.mergeDataTest([]);
 
   }
+
+  mergeDataTest(items){
+    //items = [{"category":"blog","id":"586ba9f3a36b129f1336ed38","content":"foo, bar!"},{"category":"blog","id":"586ba9f3a36b129f1336ed3c","content":"hello, world!"},{"category":"music","id":"586ba9a6dfjb129f1332ldab","content":"wow, shamwow!"}];
+    var result = items.reduce(function(accumulator, item) {
+    var cdate = Utilities.getDate(item.createdDate) as any;
+    var current = accumulator.hash[cdate.dateOnlyString];
+    
+    if(!current) {
+      current = accumulator.hash[cdate.dateOnlyString] = { 
+        saleDate: cdate.dateOnlyString,
+        items: []
+      };
+      
+      accumulator.arr.push(current);
+    }
+
+  current.items.push(item);
+  
+  return accumulator;
+}, { hash: {}, arr: [] }).arr;
+  
+console.log(result);
+return result;
+}
 
   mergeOrder(){
     this.orderService.getOrders().subscribe((data:IOrder[]) => {      
-      this.mergeData(data);
+      let groupedOrders = this.mergeDataTest(data);
+      this.createGroupedOrder(groupedOrders);
     });
   }
 
-  mergeData(data){
-    let orderdate = "";
-    for(let i=0; i <data.length; i++){
-      orderdate = Utilities.getDate(data[i].createdDate).dateOnlyString;
+  createGroupedOrder(orders : [any]){
+    for(let i =0; i < orders.length; i++){
+      this.orderService.create("order", {dayOrders: orders[i].items}, orders[i].saleDate);
     }
+  }
 
-    let order7 = [];
-    let order8 = [];
-    let orderelse = [];
-    for (let i = 0; i < data.length; i ++){
-      let m = Utilities.getMonth(data[i].createdDate);
+  mergeData(data){  
 
-      if(m === "7"){
-        order7.push(data[i]);
-      } else if(m == "8"){
-        order8.push(data[i]);
-      }
-      else{
-        orderelse.push(data[i]);
-      }
-    }
-
-    this.orderService.create("order", {"orders": order7}, "2019-07");
-    this.orderService.create("order", {"orders": order8}, "2019-08");
-    this.orderService.create("order", {"orders": orderelse}, "2019-06");
   }
 }
